2e3a28f272d8b897b75d924756a69b5e6bd482ea
Marcel Manzardo <marcello@discsox.com>
2013-12-20 15:06:52 -0800

    update config

---
+++ _includes/config.php
@@ -0,0 +1,17 @@
+<?php
+        //Personal access IDs
+        $SandBox = true;
+        $SourceName = 'MarcelManzardo';
+        $Key = '2rBGkyyJ4oddOale/+pG3riO8wg=';
+        if ($SandBox) {
+         $SiteID = '-99';
+         $UserName = 'Siteowner';
+         $UserPWD = 'apitest1234';
+        } else { //the real Thing
+         $SiteID = '5589';
+         $UserName = 'marcello@discsox.com';
+         $UserPWD = 'm1475963,M';
+        }
+
+?>
+
